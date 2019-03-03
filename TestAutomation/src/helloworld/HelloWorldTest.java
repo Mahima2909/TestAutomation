6f3d091da15af431e29edfe35fd851ba69a9cb3e
unknown <mahima.sharma2909@gmail.com>
2019-03-03 14:03:48 +0530

    sample commit

---
+++ TestAutomation/src/helloworld/HelloWorldTest.java
@@ -0,0 +1,11 @@
+package helloworld;
+import helloworld.HelloWorldVariable;
+
+public class HelloWorldTest {
+
+	public static void main(String[] args) {
+		System.out.println(HelloWorldVariable.st);
+
+	}
+
+}
---
+++ TestAutomation/src/helloworld/HelloWorldVariable.java
@@ -0,0 +1,7 @@
+package helloworld;
+
+public class HelloWorldVariable {
+	
+	public static String st = "Hello World"; 
+
+}
